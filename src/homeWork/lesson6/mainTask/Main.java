d395a7baba046f4e8de7e49ec80a9c4776f56c45
Rinat Dusumov <rinat_dusumow@int.pl>
2023-01-07 16:57:36 +0100

    commit

--- src/homeWork/lesson6/mainTask/Main.java
+++ src/homeWork/lesson6/mainTask/Main.java
@@ -6,28 +6,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // task 1
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Напишите список слов");
-//        String words = scanner.nextLine();
-//        String[] arrayWords = words.split(" ");
-//        Comparator<String> stringLength = new StringLengthSort();
-//        Arrays.sort(arrayWords, stringLength);
-//        for (String str : arrayWords) {
-//            System.out.print(str + " ");
-//        }
-//        Arrays.sort(arrayWords, Comparator.naturalOrder());
-//        System.out.println("\n" + Arrays.toString(arrayWords));
-//
-//        System.out.println("Напишите список чисел");
-//        String numbers = scanner.nextLine();
-//        String[] array = numbers.split(" ");
-//        int countNumber = array.length;
-//        Integer[] numberArray = new Integer[countNumber];
-//        for (int i = 0; i < array.length; i++) {
-//            numberArray[i] = Integer.parseInt(array[i]);
-//        }
-//        Arrays.sort(numberArray, Collections.reverseOrder());
-//        System.out.println(Arrays.toString(numberArray));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Напишите список слов");
+        String words = scanner.nextLine();
+        String[] arrayWords = words.split(" ");
+        Comparator<String> stringLength = new StringLengthSort();
+        Arrays.sort(arrayWords, stringLength);
+        for (String str : arrayWords) {
+            System.out.print(str + " ");
+        }
+        Arrays.sort(arrayWords, Comparator.naturalOrder());
+        System.out.println("\n" + Arrays.toString(arrayWords));
+
+        System.out.println("Напишите список чисел");
+        String numbers = scanner.nextLine();
+        String[] array = numbers.split(" ");
+        int countNumber = array.length;
+        Integer[] numberArray = new Integer[countNumber];
+        for (int i = 0; i < array.length; i++) {
+            numberArray[i] = Integer.parseInt(array[i]);
+        }
+        Arrays.sort(numberArray, Collections.reverseOrder());
+        System.out.println(Arrays.toString(numberArray));
 
         // task 2
         Random random = new Random();
