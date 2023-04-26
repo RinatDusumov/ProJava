cdd445d51e9eb00760fb7bac7d308cbd41c57c4d
Rinat Dusumov <rinat_dusumow@int.pl>
2023-04-26 21:05:13 +0200

    commit

--- src/main/java/students/rinat_dusumov/additionalTask/multithreading/task_6/PortDemo.java
+++ src/main/java/students/rinat_dusumov/additionalTask/multithreading/task_6/PortDemo.java
@@ -14,9 +14,10 @@ import java.util.Map;
  */
 
 public class PortDemo {
-    static volatile Map<Integer, Integer> cargoTerminalCapacity = new HashMap<>(); // вес и количество по лимиту на терминале
-    static volatile Map<Integer, Integer> stockAvailability = new HashMap<>(); // наличие на складе
-
+    private static final Map<Integer, Integer> cargoTerminalCapacity = new HashMap<>(); // вес и количество по лимиту на терминале
+    private static volatile Map<Integer, Integer> stockAvailability = new HashMap<>(); // наличие на складе
+    private static final VesselRegistration vesselRegistration = new VesselRegistration();
+    private static final PlannedWorks plannedWorks = new PlannedWorks();
     public static Map<Integer, Integer> getCargoTerminalCapacity() {
         return cargoTerminalCapacity;
     }
@@ -26,25 +27,31 @@ public class PortDemo {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        settingAFixedLimit();
+        addingTheNameOfTheProductToTheWarehouse();
+        openingOfBerthsForTheReceptionOfMerchantShips();
+        outputOfTheResultAfterTheWorkIsCompleted();
+    }
+    static void settingAFixedLimit () {
         cargoTerminalCapacity.put(1500, 100_000);
         cargoTerminalCapacity.put(5000, 100_000);
         cargoTerminalCapacity.put(3000, 100_000);
         cargoTerminalCapacity.put(10000, 100_000);
-
+    }
+    static void addingTheNameOfTheProductToTheWarehouse () {
         stockAvailability.put(1500,0);
         stockAvailability.put(3000,0);
         stockAvailability.put(5000,0);
         stockAvailability.put(10000,0);
-
-        VesselRegistration vesselRegistration = new VesselRegistration();
-        PlannedWorks plannedWorks = new PlannedWorks();
-
+    }
+    static void openingOfBerthsForTheReceptionOfMerchantShips () throws InterruptedException {
         for (int i = 0; i < 3; i++) {
             Thread thread = new Thread(new Wharf(vesselRegistration, plannedWorks), "Wharf_" + (i + 1));
             thread.start();
             thread.join();
         }
-
+    }
+    static void outputOfTheResultAfterTheWorkIsCompleted () {
         System.out.println("Остаток товара на складе: ");
         for (Map.Entry<Integer, Integer> inStock : getStockAvailability().entrySet()) {
             System.out.println(inStock);
